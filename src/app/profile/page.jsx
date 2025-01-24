b55e27c4726888d1b10741996ad5b73bde9ff982
tasnimalam1 <rafsantasnimrafsan@gmail.com>
2025-01-24 19:41:06 +0600

    protected route functionality implemented

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,20 @@
-import UserProfile from "@/components/UserProfile"
+import UserProfile from "@/components/UserProfile";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation"; // Import the redirect function
 
+const Profile = async () => {
+  const { isAuthenticated } = await getKindeServerSession();
 
-const Profile = () => {
+  if (!(await isAuthenticated())) {
+    // Redirect to the login page if not authenticated
+    redirect("/api/auth/login");
+  }
+
   return (
     <div>
-        <UserProfile></UserProfile>
+      <UserProfile />
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
